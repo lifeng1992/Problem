29022207f99608262881549a07dd49af1043d3ca
lifeng1992 <lifeng@bamcop.com>
2015-03-17 18:46:22 +0800

    SubStart-Tray
    为应用程序添加托盘图标

--- 0x02/SubStart/SubStart.cpp
+++ 0x02/SubStart/SubStart.cpp
@@ -3,10 +3,14 @@
 #include <WindowsX.h>
 #include <stdio.h>
 
+#define WM_USER_TRAY	(WM_USER + 100)
+
 #define APP_CLASS_NAME	TEXT("ClassName-SubStart")
 
 // 创建一个Windows窗口类
 void CreateWindowsClass(WNDCLASSEX *pwinclass, HINSTANCE hInstance);
+
+void InitTray(HWND hWnd, PNOTIFYICONDATA pnid);
 
 // 创建窗口过程WinProc
 LRESULT CALLBACK WinProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
@@ -41,10 +45,14 @@ LRESULT CALLBACK WinProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam){
 	PAINTSTRUCT ps;
 	HDC			hdc;
 
+	static NOTIFYICONDATA nid;
+
 	static UINT WM_USER_CLOSE = RegisterWindowMessage(TEXT("WM_USER_CLOSE"));
 
 	switch(Msg){
 		case WM_CREATE:{
+			InitTray(hWnd, &nid);
+			Shell_NotifyIconW(NIM_ADD, &nid);
 			return 0;
 		}break;
 
@@ -56,6 +64,13 @@ LRESULT CALLBACK WinProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam){
 
 		case WM_CLOSE:{
 			PostQuitMessage(0);
+			return 0;
+		}break;
+
+		case WM_USER_TRAY:{
+			if(lParam == WM_RBUTTONUP || lParam == WM_LBUTTONUP){
+				MessageBoxA(NULL, "Message from tray.", "SubStart", MB_OK);
+			}
 			return 0;
 		}break;
 
@@ -83,4 +98,20 @@ void CreateWindowsClass(WNDCLASSEX *pwinclass, HINSTANCE hInstance){
 	pwinclass->lpszMenuName		= NULL;
 	pwinclass->lpszClassName	= APP_CLASS_NAME;
 	pwinclass->hIconSm			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GITHUB));
+}
+
+// Tray Icon
+void InitTray(HWND hWnd, PNOTIFYICONDATA pnid){
+	pnid->cbSize		= sizeof(NOTIFYICONDATA);
+	pnid->hWnd			= hWnd;
+	pnid->uID			= IDI_GITHUB;
+	pnid->uFlags		= NIF_ICON | NIF_TIP | NIF_MESSAGE;
+	// Flag:NIF_ICON => hIcon
+	pnid->hIcon			= LoadIcon(GetModuleHandle(NULL), 
+		MAKEINTRESOURCE(IDI_GITHUB));
+	pnid->uVersion		= NOTIFYICON_VERSION_4;
+	// Flag:NIF_TIP => szTip
+	lstrcpy(pnid->szTip, TEXT("GitHub for Windows"));
+	// Flag:NIF_MESSAGE => NIF_MESSAGE
+	pnid->uCallbackMessage	= WM_USER_TRAY;
 }
